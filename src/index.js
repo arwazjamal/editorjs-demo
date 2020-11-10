d3068d2c0dce79db2e356e6b756edb247da4d0d5
Arwaz Jamal <arwaz.jamal@codinova.com>
2020-11-10 16:05:57 +0530

    [Fix] update webpack packages

--- src/index.js
+++ src/index.js
@@ -1,34 +1,31 @@
-// import EditorJS from '@editorjs/editorjs';
-// const Table = require('editorjs-table');
-// const List = require('editorjs-list');
-// const editor = new EditorJS({
-//     autofocus: true,
-//     data: {
-//         version: '2.18.0',
-//     },
-//     placeholder: 'click add button to get tools',
-//     holder: 'editorjs',
-//     tools: {
-//         list : {
-//             class: List,
-//             inlineToolbar: true,
-//             shortcut: 'CMD+SHIFT+L'
-//         },
-//         table: {
-//             class: Table,
-//             inlineToolbar: true,
-//             config: {
-//                 rows: 0,
-//                 cols: 0
-//             },
-//         }
-//     },
-//     data: {
-//         blocks: []
-//     }
-// })
+import EditorJS from '@editorjs/editorjs';
+const Table = require('editorjs-table');
+const List = require('editorjs-list');
+const editor = new EditorJS({
+    autofocus: true,
+    data: {
+        version: '2.18.0',
+    },
+    placeholder: 'click add button to get tools',
+    holder: 'editorjs',
+    tools: {
+        list : {
+            class: List,
+            inlineToolbar: true,
+            shortcut: 'CMD+SHIFT+L'
+        },
+        table: {
+            class: Table,
+            inlineToolbar: true,
+            config: {
+                rows: 0,
+                cols: 0
+            },
+        }
+    },
+    data: {
+        blocks: []
+    }
+})
 
-// export default editor;
-
-let test = () => console.log(123);
-test();
+export default editor;
