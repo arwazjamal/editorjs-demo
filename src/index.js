eee7af3ffd5e35e8836298a0f912e6a7201a74f2
Arwaz Jamal <arwaz.jamal@codinova.com>
2020-11-18 15:40:55 +0530

    add plugins

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ const List = require('editorjs-list');
 // plugins from editorJs
 const Checklist = require('@editorjs/checklist');
 const Marker = require('@editorjs/marker');
+const LinkTool = require('@editorjs/link');
+const Embed = require('@editorjs/embed');
+const Header = require('@editorjs/header');
+const Quote = require('@editorjs/quote');
+const SimpleImage = require('@editorjs/simple-image');
+const Warning = require('@editorjs/warning');
+const RawTool = require('@editorjs/raw');
+const Delimiter = require('@editorjs/delimiter');
+const CodeTool = require('@editorjs/code');
+const InlineCode = require('@editorjs/inline-code');
 const editor = new EditorJS({
     autofocus: true,
     data: {
@@ -34,10 +44,51 @@ const editor = new EditorJS({
         Marker: {
             class: Marker,
             shortcut: 'CMD+SHIFT+M',
-          }
+        },
+        linkTool: {
+            class: LinkTool,
+            config: {
+              endpoint: 'http://localhost:8008/fetchUrl', // Your backend endpoint for url data fetching
+            }
+        },
+        embed: Embed, 
+        header: Header,
+        quote: Quote,
+        image: SimpleImage,
+        warning: Warning,
+        raw: RawTool,
+        delimiter: Delimiter,
+        code: CodeTool,
+        inlineCode: {
+            class: InlineCode,
+            shortcut: 'CMD+SHIFT+M',
+        },
     },
     data: {
-        blocks: []
+        blocks: [
+            {
+                type : "image",
+                data : {
+                    url : "https://images.pexels.com/photos/3657423/pexels-photo-3657423.jpeg",
+                    caption : "Images for Test",
+                    withBorder : false,
+                    withBackground: false,
+                    stretched : false,
+                    width: '448px',
+                    height: '560px'
+                }
+            },
+            {
+              type: "image",
+              data: {
+                url: "https://images.pexels.com/photos/1442486/pexels-photo-1442486.jpeg",
+                caption: "Image Test",
+                withBorder: false,
+                withBackground: false,
+                stretched: false
+              }
+            },
+        ]
     }
 })
 
